5a51dce15907bad89b7dafe1dcee8039b03de34a
Braian Gomez <braiandenisgomez@gmail.com>
2022-11-04 18:02:00 -0300

    Refactor userContext.

--- src/contexts/userContext.jsx
+++ src/contexts/userContext.jsx
@@ -1,16 +1,42 @@
-import { useState, useEffect, createContext, useMemo } from 'react';
+import { useEffect, useReducer, createContext, useMemo } from 'react';
 import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
 } from '../utils/firebase/firebase.utils';
 // as the cctual value you want to access
 export const UserContext = createContext({
-  currentUser: null,
   setCurrentUser: () => null,
+  currentUser: null,
 });
 
+export const USER_ACTIONS_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTIONS_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export function UserProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTIONS_TYPES.SET_CURRENT_USER, payload: user });
+  };
 
   const value = useMemo(() => ({ currentUser, setCurrentUser }), [currentUser]);
 
