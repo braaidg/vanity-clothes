5957f4c79d119b45f6858a9cc4504182f6e08704
Braian Gomez <braiandenisgomez@gmail.com>
2022-11-04 22:52:37 -0300

    Refactored cartContext , now using Context + Reducer

--- src/contexts/userContext.jsx
+++ src/contexts/userContext.jsx
@@ -3,7 +3,9 @@ import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
 } from '../utils/firebase/firebase.utils';
-// as the cctual value you want to access
+
+import createAction from '../utils/reducer/reducer.utils';
+
 export const UserContext = createContext({
   setCurrentUser: () => null,
   currentUser: null,
@@ -35,7 +37,7 @@ export function UserProvider({ children }) {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
 
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTIONS_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTIONS_TYPES.SET_CURRENT_USER, user));
   };
 
   const value = useMemo(() => ({ currentUser, setCurrentUser }), [currentUser]);
