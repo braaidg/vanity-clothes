a58e92e59b36a13700c328c8d9a13bd65d3dfbed
Braian Gomez <braiandenisgomez@gmail.com>
2022-11-04 23:51:50 -0300

    Created store(with middleware), root reducer and user reducer

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,5 +19,6 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 0,
     'react/prop-types': 'off',
+    'default-param-last': 0,
   },
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,30 @@
-import { BrowserRouter } from 'react-router-dom';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import App from './App';
+
 import { UserProvider } from './contexts/userContext';
 import { CategoriesProvider } from './contexts/categoriesContext';
 import { CartProvider } from './contexts/cartContext';
+
+import store from './store/store';
 
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
