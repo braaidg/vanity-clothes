d1126331a828849ebd38994c1318b14d6ef6242d
Braian Gomez <braiandenisgomez@gmail.com>
2023-01-22 18:26:43 -0300

    Fetch start , success and failed action

--- src/store/category/category.action.js
+++ src/store/category/category.action.js
@@ -1,8 +1,26 @@
 import CATEGORIES_ACTIONS_TYPES from './category.types';
 
 import createAction from '../../utils/reducer/reducer.utils';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
-const setCategories = (categoriesArray) =>
-  createAction(CATEGORIES_ACTIONS_TYPES.SET_CATEGORIES, categoriesArray);
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORIES_ACTIONS_TYPES.FETCH_CATEGORIES_START);
 
-export default setCategories;
+export const fetchCategoriesSuccess = (categoriesArray) =>
+  createAction(
+    CATEGORIES_ACTIONS_TYPES.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray
+  );
+
+export const fetchCategoriesFailed = (error) =>
+  createAction(CATEGORIES_ACTIONS_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+  dispatch(fetchCategoriesStart());
+  try {
+    const categoriesArray = await getCategoriesAndDocuments();
+    dispatch(fetchCategoriesSuccess(categoriesArray));
+  } catch (error) {
+    dispatch(fetchCategoriesFailed(error));
+  }
+};
