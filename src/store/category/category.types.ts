c074d5aa6cbec3de6baef749a082be5a52f125c8
Braian Gomez <braiandenisgomez@gmail.com>
2023-01-24 03:33:51 -0300

    Typed CategoryItem and Category

--- src/store/category/category.types.ts
+++ src/store/category/category.types.ts
@@ -1,7 +1,18 @@
-enum CATEGORIES_ACTIONS_TYPES {
+export enum CATEGORIES_ACTIONS_TYPES {
   FETCH_CATEGORIES_START = 'category/FETCH_CATEGORIES_START',
   FETCH_CATEGORIES_SUCCESS = 'category/FETCH_CATEGORIES_SUCCESS',
   FETCH_CATEGORIES_FAILED = 'category/FETCH_CATEGORIES_FAILED',
 };
 
-export default CATEGORIES_ACTIONS_TYPES;
+export type CategoryItem = {
+  id: number;
+  imageUrl: string;
+  name: string;
+  price: number;
+}
+
+export type Category = {
+  title: string;
+  imageUrl: string;
+  items: CategoryItem[];
+}
